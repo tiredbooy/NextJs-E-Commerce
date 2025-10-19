05bb653d2a0aea904d854cd0bc6778e1fd986fe6
tiredbooy <mahdykazemyo1i2@gmail.com>
2025-10-19 19:09:12 +0330

    small change

--- app/_components/account/dashboard/admin/products/ManageProducts.tsx
+++ app/_components/account/dashboard/admin/products/ManageProducts.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { FaBoxes } from "react-icons/fa";
 import ProductsInformation from "./ProductsInformation";
 import ProductsTable from "./ProductsTable";
 
@@ -9,7 +11,16 @@ interface Props {
 export default function ManageProducts({}: Props) {
   return (
     <Card className="px-10 py-8 border-border bg-card">
-      <h1 className="font-semibold text-3xl">Manage Products</h1>
+      <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row gap-1 items-center">
+          <FaBoxes />
+          <h1 className="font-semibold text-base md:text-3xl">Manage Products</h1>
+        </div>
+        <Button className="font-semibold text-background cursor-pointer">
+          Add Product
+        </Button>
+      </div>
+
       {/* <div className="flex flex-row gap-5"> */}
       <ProductsInformation />
       {/* </div> */}
--- app/_components/account/dashboard/admin/users/ManageUsers.tsx
+++ app/_components/account/dashboard/admin/users/ManageUsers.tsx
@@ -1,5 +1,6 @@
 import { Card } from "@/components/ui/card";
 import UsersTable from "./UsersTable";
+import { HiUsers } from "react-icons/hi";
 
 interface Props {
   // props here
@@ -8,7 +9,10 @@ interface Props {
 export default function ManageUsers({}: Props) {
   return (
     <Card className="px-10 py-8 border-border bg-card">
-      <h1 className="font-semibold text-3xl">Manage Users</h1>
+      <div className="flex flex-row gap-1 items-center  text-2xl md:text-3xl">
+        <HiUsers />
+        <h1 className="font-semibold">Manage Users</h1>
+      </div>
       <UsersTable />
     </Card>
   );
